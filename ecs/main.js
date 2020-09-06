a6c484fa5f24049c155c42377f43a78f2e400abb
Anders Busch <andersbusch@gmail.com>
2020-09-06 17:38:11 +0200

    added map instead of linear runthrough

--- ecs/main.js
+++ ecs/main.js
@@ -10,10 +10,43 @@ let entityIdGenerator = new class {
   }
 }
 
-class Entity extends Array {
+let componentIdGenerator = new class {
+  constructor() {
+    this._id = 0;
+  }
+
+  get next() {
+    return this._id++;
+  }
+}
+
+class Entity {
   constructor(id, ...args) {
-    super(...args);
+    this.components = new Map();
     this.id = id;
+    for (let c of args) {
+      this.components.set(c.cid, c);
+    }
+  }
+
+  getComponentById(componentId) {
+    return this.components.get(componentId);
+  }
+
+  hasComponentId(componentId) {
+    return this.components.has(componentId);
+  }
+
+  removeComponentById(componentId) {
+    this.components.remove(componentId);
+  }
+
+  addComponent(componentInstance) {
+    this.components.set(componentInstance.cid, componentInstance)
+  }
+
+  get length() {
+    return this.components.size
   }
 };
 
@@ -22,20 +55,38 @@ class PositionComponent {
     this.x = x || 0;
     this.y = y || 0;
   }
+
+  get cid() {
+    return PositionComponent.cid;
+  }
 };
+
+PositionComponent.cid = componentIdGenerator.next;
 
 class VelocityComponent {
   constructor(x, y) {
     this.x = x || 0;
     this.y = y || 0;
   }
+
+  get cid() {
+    return VelocityComponent.cid;
+  }
 };
+
+VelocityComponent.cid = componentIdGenerator.next;
 
 class NamedComponent {
   constructor(name) {
     this.name = name;
   }
+
+  get cid() {
+    return NamedComponent.cid;
+  }
 };
+
+NamedComponent.cid = componentIdGenerator.next;
 
 class EntitiesManager extends Array {
   newEntity(...components) {
@@ -50,20 +101,14 @@ class EntitiesManager extends Array {
       if (componentList.length > entity.length) {
         continue;  // entity's component list is not a superset
       }
-      let componentSet = new Entity(entity.id);
-      let cindex = 0;
-      for (let componentType of componentList) {
-        for (let component of entity) {
-          if (component instanceof componentType) {
-            componentSet[cindex] = component;  // using cindex guaranties the
-                                               // ordering of the components
-          }
-        }
-        cindex += 1;
+
+      let shouldAdd = true;
+      for (let c of componentList) {
+        shouldAdd = shouldAdd && entity.components.has(c.cid);
       }
-      if (componentSet.length === componentList.length) {
-        results.push(componentSet);  // we are only interested in components
-                                     // that have all of the input components
+
+      if (shouldAdd) {
+        results.push(entity)
       }
     }
     return results;
@@ -78,7 +123,8 @@ class PhysicalSystem {
     let es =
         entities.getEntitiesByComponents(PositionComponent, VelocityComponent);
     for (let entity of es) {
-      let [pc, vc] = entity;
+      let pc = entity.getComponentById(PositionComponent.cid);
+      let vc = entity.getComponentById(VelocityComponent.cid);
       pc.x += vc.x * dt;
       pc.y += vc.y * dt;
     }
@@ -87,30 +133,29 @@ class PhysicalSystem {
 
 
 (function main() {
-  // let physical = new PhysicalSystem()
+  let physical = new PhysicalSystem()
 
-  entities.newEntity(
-      new NamedComponent(), new PositionComponent(),
-      new VelocityComponent(2, 3));
+
   entities.newEntity(new PositionComponent(), new VelocityComponent(1, 1));
   entities.newEntity(new PositionComponent());
 
-
-  console.log(entities.getEntitiesByComponents(PositionComponent, VelocityComponent))
+  console.log(
+      entities.getEntitiesByComponents(PositionComponent, VelocityComponent))
 
   console.log(entities.getEntitiesByComponents(PositionComponent))
 
-  for (let e of entities.getEntitiesByComponents(PositionComponent, VelocityComponent)) {
-    let [pc, vc] = e;
-    console.log(pc, vc)
+  for (let e of entities.getEntitiesByComponents(
+           PositionComponent, VelocityComponent)) {
+    let pc = e.getComponentById(PositionComponent.cid);
+    let vc = e.getComponentById(VelocityComponent.cid);
+    console.log("components", pc, vc)
   }
 
-  
-  /*
+
   physical.updateVelocities();
   for (let e of entities) {
     console.log('->', e)
   }
-  */
+  
   console.log(entities.length)
 })();
